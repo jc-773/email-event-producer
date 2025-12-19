09a94904e79766973a3a99eca43f5c82371d0d62
jc-773 <jonc960@gmail.com>
2025-12-19 07:52:22 -0500

    init commit

---
+++ src/main/java/com/email_event/email_producer/EmailProducerApplication.java
@@ -0,0 +1,13 @@
+package com.email_event.email_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/email_event/email_producer/EmailProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email_event.email_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
